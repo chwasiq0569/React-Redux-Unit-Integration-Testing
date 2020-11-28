07fc32a2167d3b250e8732df9f4ada82776034e0
unknown <samsunggrandprimeplus056@gmail.com>
2020-11-28 03:29:47 -0800

    Header Component Tests

--- src/Components/Header/header.test.js
+++ src/Components/Header/header.test.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Header from "./index";
-
+import { findTestbyAttr } from "../../Utils/index";
 // it() is same as test()
-
 const setUp = (props = {}) => {
   //default parameter
   const component = shallow(<Header {...props} />);
@@ -17,31 +16,11 @@ describe("Testing Components", () => {
   });
 
   it("Component have only one class named headerComponent", () => {
-    const wrapper = component.find(`[data-test='headerComponent']`);
+    const wrapper = findTestbyAttr(component, "headerComponent");
     expect(wrapper.length).toBe(1);
   });
   it("Should render a logo", () => {
-    const wrapper = component.find(`[data-test='logoText']`);
+    const wrapper = findTestbyAttr(component, "logoText");
     expect(wrapper.length).toBe(1);
   });
 });
-
-// import { render, screen } from "@testing-library/react";
-// import App from "./App";
-
-// test("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-//////////////////////////////////////
-
-// import { render, screen } from "@testing-library/react";
-// import App from "./App";
-
-// test("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
